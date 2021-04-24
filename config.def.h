124e596814d45228b4ff85abd5a3060259f4388f
Rasa <rasa.hosseinzadeh@gmail.com>
2021-04-24 19:44:28 -0400

    Change default font and colors.

--- config.def.h
+++ config.def.h
@@ -5,13 +5,17 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	 "Hack:size=14:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_dark[]   = "#282c34";
+static const char col_light[]  = "#d7d7d7";
+static const char col_red[]    = "#924441";
+static const char col_gray[]   = "#404040";
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { col_light, col_dark, col_red },
+	[SchemeSel]  = { col_light, col_red, col_red },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -27,4 +31,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
